4ebc24ca12d523fbe4e4aa8387c8bccbcb70c038
Nicolae Tinco <ntinco@ebizlatin.com>
2023-04-05 10:23:56 -0500

    Corrección, funcionar ambos maps

--- Osinergmin-web/js/map_electricidad.js
+++ Osinergmin-web/js/map_electricidad.js
@@ -25,221 +25,249 @@ function btnOpenContentDataMap(codigo, nombre, ubigeo) {
 function filterData(data, filtro) {
   $("#tbdReporte").html("");
   var filter = data.filter(t => t.pais == filtro);
-  filter.forEach(t => {
-    $("#tbdReporte").append("<tr>"+
-    "<td>"+t.codigo+"<td>"+
-    "<td>"+t.pais+"<td>"+
-    "<td>"+ monthNames[new Date(t.mes).getMonth()] + " - "+ new Date(t.mes).getFullYear() +"<td>"+
-    "<td>"+t.unidad30.toFixed(2)+"<td>"+
-    "<td>"+t.unidad65.toFixed(2)+"<td>"+
-    "<td>"+t.unidad125.toFixed(2)+"<td>"+
-    "<td>"+t.unidad300.toFixed(2)+"<td>"+
-    "<td>"+t.unidad1000.toFixed(2)+"<td>"+
-    "<td>"+t.unidad50000.toFixed(2)+"<td>"+
-    "<td>"+t.unidad500000.toFixed(2)+"<td>"+
-    "</tr>");
-  });
+
+  if (filter.length > 0) {
+    filter.forEach(t => {
+      $("#tbdReporte").append("<tr>" +
+        "<td>" + t.codigo + "</td>" +
+        "<td>" + t.pais + "</td>" +
+        "<td>" + monthNames[new Date(t.mes).getMonth()] + " - " + new Date(t.mes).getFullYear() + "</td>" +
+        "<td>" + t.unidad30.toFixed(2) + "</td>" +
+        "<td>" + t.unidad65.toFixed(2) + "</td>" +
+        "<td>" + t.unidad125.toFixed(2) + "</td>" +
+        "<td>" + t.unidad300.toFixed(2) + "</td>" +
+        "<td>" + t.unidad1000.toFixed(2) + "</td>" +
+        "<td>" + t.unidad50000.toFixed(2) + "</td>" +
+        "<td>" + t.unidad500000.toFixed(2) + "</td>" +
+        "</tr>");
+    });
+  } else {
+    $("#tbdReporte").append("<tr>" +
+      "<td colspan='10'>Sin información...</td>" +
+      "</tr>");
+  }
+
+
 }
 require([
-    "esri/core/urlUtils",
-    "esri/Map",
-    "esri/config",
-    "esri/views/MapView",
-    "esri/WebMap",
-    "esri/layers/FeatureLayer",
-    "esri/tasks/QueryTask",
-    "esri/tasks/support/Query",
-    "esri/widgets/Expand",
-    "esri/widgets/BasemapGallery",
-    "esri/widgets/Editor",
-	  "esri/widgets/Search",
-	  "esri/widgets/Home",
-    "esri/widgets/LayerList",
-    "esri/layers/MapImageLayer"
-    ], (
-        urlUtils,
-        Map,
-        esriConfig,
-        MapView,
-        WebMap,
-        FeatureLayer,
-        QueryTask,
-        Query,
-        Expand,
-        BasemapGallery, 
-        Editor,
-		    Search,
-		    Home,
-        LayerList,
-        MapImageLayer
-        ) => {
-        
-        //_proxyurl = "https://gisem.osinergmin.gob.pe/proxy_developer/proxy.ashx";
-        _proxyurl = "";
-        $(document).ready(function(){
+  "esri/core/urlUtils",
+  "esri/Map",
+  "esri/config",
+  "esri/views/MapView",
+  "esri/WebMap",
+  "esri/layers/FeatureLayer",
+  "esri/tasks/QueryTask",
+  "esri/tasks/support/Query",
+  "esri/widgets/Expand",
+  "esri/widgets/BasemapGallery",
+  "esri/widgets/Editor",
+  "esri/widgets/Search",
+  "esri/widgets/Home",
+  "esri/widgets/LayerList",
+  "esri/layers/MapImageLayer"
+], (
+  urlUtils,
+  Map,
+  esriConfig,
+  MapView,
+  WebMap,
+  FeatureLayer,
+  QueryTask,
+  Query,
+  Expand,
+  BasemapGallery,
+  Editor,
+  Search,
+  Home,
+  LayerList,
+  MapImageLayer
+) => {
 
-          let urlparams= window.location.search;
-          _globalidor = urlparams.substring(1);
-          code = _globalidor.split('=')[1];
-          console.log(code);
-          if (code ){           
-            code = code.substring(0, 2);
-            where = "CODDEPARTAMENTO='"+code.substring(0, 2)+"'";            
-          }
-          if (code != "00"){
-            $("#divCompleto").hide();
-            $('#divContent').hide();
-            $('#iframeMap').show();
-            $('.btn-map-return').hide();
-            $('#iframe-electricidad').attr('src', tempUri+code+'0000');
-          }
-          
-          map = new Map({
-              basemap: "osm"
-          });
+  //_proxyurl = "https://gisem.osinergmin.gob.pe/proxy_developer/proxy.ashx";
+  _proxyurl = "";
+  $(document).ready(function () {
 
-          view = new MapView({
-              container: "map",
-              map: map,
-              center: [-74.049, -8.185],
-              zoom: 6
-          });
+    let urlparams = window.location.search;
+    _globalidor = urlparams.substring(1);
+    code = _globalidor.split('=')[1];
+    console.log(code);
+    if (code) {
+      code = code.substring(0, 2);
+      where = "CODDEPARTAMENTO='" + code.substring(0, 2) + "'";
+    }
+    if (code != "00") {
+      $("#divCompleto").hide();
+      $('#divContent').hide();
+      $('#iframeMap').show();
+      /* $('.btn-map-return').hide(); */
+      $('#iframe-electricidad').attr('src', tempUri + code + '0000');
+    }
 
-          function changeSrcIframe(uriIframe) {
-            $('#iframe-electricidad').attr("src", uriIframe);
-          }
-          
-          $("img[usemap]:last").mapify({
-            popOver: {
-              content: function (zone) {
-                changeSrcIframe(tempUri + zone.attr("ubigeo"));
-                return "<strong>" + zone.attr("data-title") + "</strong>";
-              },
-              delay: 0.3,
-              margin: "15px",
-              height: "130px",
-              width: "260px"
-            },
-            onAreaHighlight: function () {
-              console.log("onAreaHighlight callback");
-            },
-            onMapClear: function () {
-              console.log("onMapClear callback");
-            }
-          });
-          
-          $("#dynamicClassChange").click(function () {
-            $(this).attr("data-hover-class", "hover-green");
-          });
-
-          let basemapGallery = new BasemapGallery({
-              view: view
-          });
-          
-          //PROXY//
-          //Descomentar para producción//
-          //urlUtils.addProxyRule({
-          //    urlPrefix: "https://services5.arcgis.com/oAvs2fapEemUpOTy",
-          //    proxyUrl: _proxyurl
-          //});
-
-          const MeExpand = new Expand({
-              view: view,
-              content: basemapGallery,
-              expanded: false,
-              expandTooltip: 'Mapas Base'
-          });
-			
-          var homeWidget = new Home({
-            view: view
-          });
-
-          let layerList = new LayerList({
-            view: view
-          });
-
-          const MeExpandLayer = new Expand({
-            view: view,
-            content: layerList,
-            expanded: false,
-            expandTooltip: 'Capas'
-          });         
-          
-          const searchWidget = new Search({
-            view: view
-          });
-
-          view.ui.add(searchWidget, { position: "top-right", index: 2 });
-          view.ui.add(homeWidget, "top-right");
-          view.ui.add(MeExpand, 'top-right');  
-          view.ui.add(MeExpandLayer, { position: "top-right"});
-
-          const featureLayer = new FeatureLayer({
-            url: "https://gisem.osinergmin.gob.pe/serverosih/rest/services/Cartografia/LIMITE_DEPARTAMENTAL/MapServer/0",
-            outFields: ["*"],
-            definitionExpression: where
-          });
-      
-          const layer2 = new MapImageLayer({
-            url: "https://gisem.osinergmin.gob.pe/serverosih/rest/services/Electricidad/MapaSEIN_Operacion/MapServer"
-          });
-      
-          const layer3 = new MapImageLayer({
-            url: "https://gisem.osinergmin.gob.pe/serverosih/rest/services/Electricidad/MapaSEIN_Proyectadas/MapServer"
-          });
-
-
-          map.add(layer2);
-          map.add(layer3);
-          map.add(featureLayer);
-          $("#map").css("height", "100%");
-
-          featureLayer.queryFeatures().then(results => {
-            if (results.features.length > 0)
-              zoomToLayer2(results);
-          });
-
-
-          function zoomToLayer2(results, _zoom){
-            var sourceGraphics = results.features.map(e => { return e.geometry });
-            view.goTo(sourceGraphics);
-          }
-
-          $("#ddlPais").on('change', function (e) {
-            console.log(e.currentTarget.value);
-            if (e.currentTarget.value != "")
-              filterData(responseReporte, e.currentTarget.value);
-          });
-
-          function filterData(data, filtro){
-            $("#tbdReporte").html("");
-            var filter = data.filter(t => t.codigo == filtro);
-            filter.forEach(t => {
-              $("#tbdReporte").append("<tr>"+
-              "<td>"+t.codigo+"<td>"+
-              "<td>"+t.pais+"<td>"+
-              "<td>"+ monthNames[new Date(t.mes).getMonth()] + " - "+ new Date(t.mes).getFullYear() +"<td>"+
-              "<td>"+t.unidad30.toFixed(2)+"<td>"+
-              "<td>"+t.unidad65.toFixed(2)+"<td>"+
-              "<td>"+t.unidad125.toFixed(2)+"<td>"+
-              "<td>"+t.unidad300.toFixed(2)+"<td>"+
-              "<td>"+t.unidad1000.toFixed(2)+"<td>"+
-              "<td>"+t.unidad50000.toFixed(2)+"<td>"+
-              "<td>"+t.unidad500000.toFixed(2)+"<td>"+
-              "</tr>");
-            });            
-          }
-
-          fetch("https://gisem.osinergmin.gob.pe/validar/observatorio3/apiObservatorio/api")
-          .then((response) => response.json())
-          .then((response) => {
-            //console.log(response.data);
-            responseReporte = response.data;
-            $("#ddlPais").html("<option value=''>Seleccione</option>");
-            responseReporte.forEach(t => {
-              $("#ddlPais").append("<option value="+t.codigo+">"+t.pais+"</option>");
-            });           
-            filterData(responseReporte, "PE");
-          });
-        });
+    map = new Map({
+      basemap: "osm"
     });
+
+    view = new MapView({
+      container: "map",
+      map: map,
+      center: [-74.049, -8.185],
+      zoom: 6
+    });
+
+    function changeSrcIframe(uriIframe) {
+      $('#iframe-electricidad').attr("src", uriIframe);
+    }
+
+    $("#firstMap[usemap]:last").mapify({
+      popOver: {
+        content: function (zone) {
+          changeSrcIframe(tempUri + zone.attr("ubigeo"));
+          return "<strong>" + zone.attr("data-title") + "</strong>";
+        },
+        delay: 0.3,
+        margin: "15px",
+        height: "130px",
+        width: "260px"
+      },
+      onAreaHighlight: function () {
+        console.log("onAreaHighlight callback");
+      },
+      onMapClear: function () {
+        console.log("onMapClear callback");
+      }
+    });
+
+    $("#secondMap[usemap]:last").mapify({
+      popOver: {
+        content: function (zone) {
+          changeSrcIframe(tempUri + zone.attr("ubigeo"));
+          return "<strong>" + zone.attr("data-title") + "</strong>";
+        },
+        delay: 0.3,
+        margin: "15px",
+        height: "130px",
+        width: "260px"
+      },
+      onAreaHighlight: function () {
+        console.log("onAreaHighlight callback");
+      },
+      onMapClear: function () {
+        console.log("onMapClear callback");
+      }
+    });
+
+    $("#dynamicClassChange").click(function () {
+      $(this).attr("data-hover-class", "hover-green");
+    });
+
+    let basemapGallery = new BasemapGallery({
+      view: view
+    });
+
+    //PROXY//
+    //Descomentar para producción//
+    //urlUtils.addProxyRule({
+    //    urlPrefix: "https://services5.arcgis.com/oAvs2fapEemUpOTy",
+    //    proxyUrl: _proxyurl
+    //});
+
+    const MeExpand = new Expand({
+      view: view,
+      content: basemapGallery,
+      expanded: false,
+      expandTooltip: 'Mapas Base'
+    });
+
+    var homeWidget = new Home({
+      view: view
+    });
+
+    let layerList = new LayerList({
+      view: view
+    });
+
+    const MeExpandLayer = new Expand({
+      view: view,
+      content: layerList,
+      expanded: false,
+      expandTooltip: 'Capas'
+    });
+
+    const searchWidget = new Search({
+      view: view
+    });
+
+    view.ui.add(searchWidget, { position: "top-right", index: 2 });
+    view.ui.add(homeWidget, "top-right");
+    view.ui.add(MeExpand, 'top-right');
+    view.ui.add(MeExpandLayer, { position: "top-right" });
+
+    const featureLayer = new FeatureLayer({
+      url: "https://gisem.osinergmin.gob.pe/serverosih/rest/services/Cartografia/LIMITE_DEPARTAMENTAL/MapServer/0",
+      outFields: ["*"],
+      definitionExpression: where
+    });
+
+    const layer2 = new MapImageLayer({
+      url: "https://gisem.osinergmin.gob.pe/serverosih/rest/services/Electricidad/MapaSEIN_Operacion/MapServer"
+    });
+
+    const layer3 = new MapImageLayer({
+      url: "https://gisem.osinergmin.gob.pe/serverosih/rest/services/Electricidad/MapaSEIN_Proyectadas/MapServer"
+    });
+
+
+    map.add(layer2);
+    map.add(layer3);
+    map.add(featureLayer);
+    $("#map").css("height", "100%");
+
+    featureLayer.queryFeatures().then(results => {
+      if (results.features.length > 0)
+        zoomToLayer2(results);
+    });
+
+
+    function zoomToLayer2(results, _zoom) {
+      var sourceGraphics = results.features.map(e => { return e.geometry });
+      view.goTo(sourceGraphics);
+    }
+
+    $("#ddlPais").on('change', function (e) {
+      console.log(e.currentTarget.value);
+      if (e.currentTarget.value != "")
+        filterData(responseReporte, e.currentTarget.value);
+    });
+
+    function filterData(data, filtro) {
+      $("#tbdReporte").html("");
+      var filter = data.filter(t => t.codigo == filtro);
+      filter.forEach(t => {
+        $("#tbdReporte").append("</tr>" +
+          "<td>" + t.codigo + "</td>" +
+          "<td>" + t.pais + "</td>" +
+          "<td>" + monthNames[new Date(t.mes).getMonth()] + " - " + new Date(t.mes).getFullYear() + "</td>" +
+          "<td>" + t.unidad30.toFixed(2) + "</td>" +
+          "<td>" + t.unidad65.toFixed(2) + "</td>" +
+          "<td>" + t.unidad125.toFixed(2) + "</td>" +
+          "<td>" + t.unidad300.toFixed(2) + "</td>" +
+          "<td>" + t.unidad1000.toFixed(2) + "</td>" +
+          "<td>" + t.unidad50000.toFixed(2) + "</td>" +
+          "<td>" + t.unidad500000.toFixed(2) + "</td>" +
+          "</tr>");
+      });
+    }
+
+    fetch("https://gisem.osinergmin.gob.pe/validar/observatorio3/apiObservatorio/api")
+      .then((response) => response.json())
+      .then((response) => {
+        //console.log(response.data);
+        responseReporte = response.data;
+        $("#ddlPais").html("<option value=''>Seleccione</option>");
+        responseReporte.forEach(t => {
+          $("#ddlPais").append("<option value=" + t.codigo + ">" + t.pais + "</option>");
+        });
+        filterData(responseReporte, "PE");
+      });
+  });
+});
